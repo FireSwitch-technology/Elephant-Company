a824962b237a1b89a7a2ef6e211a744afe4450c8
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2025-01-31 12:06:29 +0100

    updatenew

--- src/components/Ourservice.jsx
+++ src/components/Ourservice.jsx
@@ -1,9 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import servicedata from "../Data/servicedata";
+import video1 from "../assets/materials/homereno.mp4";
+import video2 from "../assets/materials/plastering.mp4";
+import video3 from "../assets/materials/interior.mp4";
+import video4 from "../assets/materials/commreno.mp4";
+import video5 from "../assets/materials/Floorinstallation.mp4";
 
 const Ourservice = () => {
+  const videoRefs = useRef([]);
+
+  const handleMouseEnter = (index) => {
+    videoRefs.current[index].play();
+  };
+
+  const handleMouseLeave = (index) => {
+    videoRefs.current[index].pause();
+    videoRefs.current[index].currentTime = 0; // Reset video to the beginning
+  };
+
+  const videoBg = [
+    { id: 1, video: video2, name: "Home Renovations" },
+    { id: 2, video: video4, name: "Interior & Exterior Painting" },
+    { id: 3, video: video5, name: "Flooring Installation" },
+    { id: 4, video: video3, name: "Commercial Renovations" },
+    { id: 5, video: video2, name: "Drywall & Plastering" },
+  ];
   useEffect(() => {
     AOS.init({
       offset: 200,
@@ -26,17 +49,29 @@ const Ourservice = () => {
         {/*  */}
 
         <div className="flex flex-wrap justify-evenly items-center  gap-3 ">
-          {servicedata.map(({ id, image, name }) => (
+          {videoBg.map(({ id, video, name }, index) => (
             <div
               key={id}
-              className=" relative max-w-[480px] h-[490px] rounded-3xl "
+              data-aos="zoom-in"
+              data-aos-delay="200"
+              className=" relative w-[450px] h-[500px] rounded-3xl "
             >
-              <img
-                src={image}
-                alt=""
-                className=" w-full h-full object-cover rounded-3xl "
+              <div className="absolute  top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-black to-transparent opacity-50 rounded-3xl"></div>
+              <video
+                src={video}
+                ref={(el) => (videoRefs.current[index] = el)}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={() => handleMouseLeave(index)}
+                loop
+                muted
+                className=" w-full h-full object-cover rounded-3xl"
               />
-              <p className="absolute bottom-3 left-5 text-3xl font-display font-medium  text-white ">
+
+              <p
+                className={`absolute ${
+                  index === 0 ? "top-0" : "bottom-0"
+                }  left-5 text-3xl font-display font-medium  text-white `}
+              >
                 {name}{" "}
               </p>
             </div>
