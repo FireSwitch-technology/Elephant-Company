cd65f0fa055e31024d140a46b12d45f1bebff741
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2025-02-02 21:51:17 +0100

    updatenew

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full py-2  bg-black-300">
-      <p className="font-display font-normal text-xl text-white text-center">
+      <p className="font-display font-normal text-base sm:text-xl text-white text-center">
         ©️2025 Elephant Company INC. All Rights Reserved.
       </p>
     </div>
